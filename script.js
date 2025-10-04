794ef51bc673a31af6d665e246f6faefaf32d2d0
Misfit <78898735+Misfit64@users.noreply.github.com>
2025-10-04 11:41:20 +0530

    Add initial JavaScript and CSS for Etch-a-Sketch grid layout

---
+++ script.js
@@ -0,0 +1,17 @@
+let container = document.querySelector("div.container");
+function createDiv(number){
+    let newContainer = document.createElement("div");
+    newContainer.classList.add("row");
+    for(k = 0; k < number; k++){
+        let newDiv = document.createElement("div");
+        newDiv.textContent ="test";
+        newContainer.append(newDiv);
+    }
+    container.append(newContainer);
+}
+function multipleDivs(rows = 1,columns = 1){
+    for(i = 0; i < rows; i++){
+        createDiv(columns);
+    }
+}
+multipleDivs();
